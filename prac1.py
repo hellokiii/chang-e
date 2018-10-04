b71acf20574189bd5b647d6f3d21bf2e682f1721
ghstream <32242949+ghstream@users.noreply.github.com>
2018-10-04 10:08:41 +0900

    Update prac1.py

--- prac1.py
+++ prac1.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from PIL import Image
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
-from keras.convolutional import Conv2D, MaxPooling2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
 
@@ -16,28 +16,33 @@ def load_lego_data():
         for filenames in files:
             if os.path.splitext(filenames)[1] == '.png':
                 image = Image.open('{image}'.format(image=os.path.join(root, filenames)))
-                png_to_rgb.append(np.array(image)[:, :, :3])
-                label.append([int(root[-1])] if root[-2] == '\\' else [int(root[-2:])])
+                png_to_rgb.append(np.array(image)[:, :, :3].astype('uint8'))
+                label.append([int(root[-1])] if root[-2] == '/' else [int(root[-2:])])
     return png_to_rgb, label
 
 def prac_model(num_classes):
     # Create model
     model = Sequential()
-    model.add(Conv2D(7, (5, 5), input_shape=(200, 200, 3), activation='relu'))
+    model.add(Conv2D(11, (5, 5), input_shape=(200, 200, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(3, 3)))
-    model.add(Conv2D(17, (5, 5), activation='relu'))
+    model.add(Conv2D(27, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(3, 3)))
     model.add(Dropout(0.2))
     model.add(Flatten())
-    model.add(Dense(150, activation='relu'))
-    model.add(Dense(37, activation='relu'))
+    model.add(Dense(170, activation='relu'))
+    model.add(Dense(50, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
 if __name__ == '__main__':
-    X_origianl, y_original = load_lego_data()
-    X_train, X_test, y_train, y_test = train_test_split(X_original, y_original, test_size=0.2, random_state=41)
+    X_original, y_original = load_lego_data()
+    X_train, X_test, y_train, y_test = train_test_split(X_original, y_original,
+                                        test_size=0.2, random_state=41)
+    X_train = np.asarray(X_train)
+    X_test = np.asarray(X_test)
+    y_train = np.asarray(y_train)
+    y_test = np.asarray(y_test)
     # Reshape to be [# of samples][width][height][pixels]
     X_train = X_train.reshape(X_train.shape[0], 200, 200, 3).astype('float32')
     X_test = X_test.reshape(X_test.shape[0], 200, 200, 3).astype('float32')
@@ -47,16 +52,15 @@ if __name__ == '__main__':
     # One-hot encoded outputs
     y_train = np_utils.to_categorical(y_train)
     y_test = np_utils.to_categorical(y_test)
-    num_classes = y_test.shape[1]
+    #num_classes = y_test.shape[1]
 
     # Build the model
-    model = prac_model(num_classes=num_classes)
-
+    model = prac_model(num_classes=16)
     # Fit the model
     model.fit(X_train, y_train, validation_data=(X_test, y_test),
-                epochs=10, batch_size=200, verbose=2)
+                epochs=18, batch_size=50, verbose=2)
     scores = model.evaluate(X_test, y_test, verbose=0)
-    print("Baseline Error: $.2f%%" % (100 - scores[1]*100))
+    print("Baseline Error: %.2f%%" % (100 - scores[1]*100))
 
     test_image_index = 15
     prediction = model.predict(X_test[test_image_index].reshape(1, 200, 200, 3))
@@ -65,7 +69,7 @@ if __name__ == '__main__':
 
     # Print X_test[test_image_index].shape
     plt.title('Label is {label}, Prediction is {pred}'.format(label=label, pred=prediction))
-    plt.imshow(X_test[test_image_index].reshape(200, 200), cmap='gray')
+    plt.imshow(X_test[test_image_index].reshape(200, 200, 3), cmap='gray')
     plt.show()
 
-quit()
+    quit()
